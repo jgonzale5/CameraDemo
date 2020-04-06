3d599a031700aae901851f05f1d1b751e2d035ef
JohnathanWells <slaifir@hotmail.com>
2020-04-06 00:59:06 -0400

    First person controller

--- Assets/FirstPersonCamera.cs
+++ Assets/FirstPersonCamera.cs
@@ -4,24 +4,39 @@ using UnityEngine;
 
 public class FirstPersonCamera : MonoBehaviour
 {
-    public float turningSpeed = 10f;
-    public KeyCode leftTurnKey;
-    public KeyCode rightTurnKey;
+    public float mouseSensitivity = 100f;
+    public Transform playerCamera;
+
+    float minClampingValue = -90f;
+    float maxClampingValue = 90f;
+
+    float lookYRotation = 0;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 currentRotation = this.transform.eulerAngles;
 
-        if (Input.GetKey(leftTurnKey))
-        {
-            currentRotation.y -= turningSpeed * Time.deltaTime;
-        }
-        else if (Input.GetKey(rightTurnKey))
-        {
-            currentRotation.y += turningSpeed * Time.deltaTime;
-        }
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        
+        currentRotation.y += mouseX * mouseSensitivity * Time.deltaTime;
 
         this.transform.eulerAngles = currentRotation;
+
+
+        float yInput = -mouseY * mouseSensitivity * Time.deltaTime;
+
+        lookYRotation += yInput;
+
+        lookYRotation = Mathf.Clamp(lookYRotation, minClampingValue, maxClampingValue);
+
+        playerCamera.localRotation = Quaternion.Euler(lookYRotation, 0, 0);
     }
 }
